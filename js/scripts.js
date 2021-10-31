9da95b0192d69d4afa5312f6253b8edaba362559
Katie Pundt <kzpundt@gmail.com>
2021-10-30 18:40:44 -0700

    create new prototype method to add contacts to the address book

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ function AddressBook() {
   this.contacts = {};
 }
 
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts[contact.firstName] = contact;
+};
+
 // Business logic for Contacts
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;

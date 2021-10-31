554625d951c41cbbb1d8913d23fa831a5717b98a
Katie Pundt <kzpundt@gmail.com>
2021-10-30 18:37:48 -0700

    create AddressBook constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+// Business logic for AddressBook
+function AddressBook() {
+  this.contacts = {};
+}
+
+// Business logic for Contacts
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
